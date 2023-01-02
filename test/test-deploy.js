b118625ad4a24fca627d81d6658202f6abf6439a
solextin <106186983+solextin@users.noreply.github.com>
2023-01-02 10:59:22 +0100

    some more robust test

--- test/test-deploy.js
+++ test/test-deploy.js
@@ -21,10 +21,26 @@ describe("SimpleStorage", function () {
     const favoriteNumber = await simpleStorage.retrieve();
     expect(favoriteNumber.toString()).to.equal(newValue)
   })
-  // it("Should add person to the people array", async() => {
-  //   const initialLength = await simpleStorage.people();
-  //   const txResp = await simpleStorage.addPerson("celestine", "7");
-  //   await txResp.wait(1);
-
-  // })
+  it("Should add person to the people array", async function() {
+      const expectedName = "celestine"
+      const expectedFavoriteNum = "7"
+      const txResp = await simpleStorage.addPerson(
+          expectedName,
+          expectedFavoriteNum
+      )
+      await txResp.wait(1)
+      const { name, favoriteNumber } = await simpleStorage.people(0);
+      // const respNum = await simpleStorage.nameToFavoriteNumber[name];
+      // console.log(respNum)
+      /**
+       * This could be done this way
+       */
+      // const person = await simpleStorage.people(0);
+      // const name = person.name
+      // const favoriteNumber = person.favoriteNumber
+      assert.equal(name, expectedName)
+      assert.equal(favoriteNumber.toString(), expectedFavoriteNum)
+      // assert.equal(respNum, favoriteNumber)
+      
+  })
 });
